94f9917cc604de78facec7ce4b0cfc6bf688b801
adriandallegrave <adriandallegrave@gmail.com>
2021-08-25 13:33:02 -0300

    feat: Add function to require user to be logged in

---
+++ helpers.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+# Function to redirect users to login page when not logged in
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
